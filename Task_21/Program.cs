b912082f575b29efc4afea7930e9e41fbc0f19cc
Vitaliy Zalutskiy <zal.vitali@gmail.com>
2023-02-08 17:54:28 +0300

    Tasks

---
+++ Task_21/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+Console.WriteLine("Для поиска расстояния между точками в пространстве 3D, введите их координаты:");
+Console.Write("A(x): ");
+int ax = int.Parse(Console.ReadLine()!);
+Console.Write("A(y): ");
+int ay = int.Parse(Console.ReadLine()!);
+Console.Write("A(z): ");
+int az = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("");
+
+Console.Write("B(x): ");
+int bx = int.Parse(Console.ReadLine()!);
+Console.Write("B(y): ");
+int by = int.Parse(Console.ReadLine()!);
+Console.Write("B(z): ");
+int bz = int.Parse(Console.ReadLine()!);
+
+double dx2 = Math.Pow(Convert.ToDouble(ax - bx), 2);
+double dy2 = Math.Pow(Convert.ToDouble(ay - by), 2);
+double dz2 = Math.Pow(Convert.ToDouble(az - bz), 2);
+
+double result = Math.Round(Math.Sqrt(dx2 + dy2 + dz2), 2);
+
+Console.WriteLine($"Расстояние между точками равно {result}");
---
+++ Task_23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Получим последовательность кубов числе от 1 до N. Введите число N:");
+
+int numbern = int.Parse(Console.ReadLine()!);
+
+for(int i = 1; i <= numbern; i++)
+{
+    int i3 = i * i * i;
+    Console.Write(i3);
+
+    if (i < numbern) 
+    {
+        Console.Write(", ");
+    }
+
+    else
+    {
+        Console.WriteLine("");
+    }
+}
